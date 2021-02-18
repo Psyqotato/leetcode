7cef4bdc984bbdb19b868614729e6ed8674c2b7e
PotatOceanus <39735195+PotatOceanus@users.noreply.github.com>
2021-02-19 01:05:14 +1100

    Add files via upload

--- 0063.py
+++ 0063.py
@@ -9,7 +9,6 @@ class Solution:
         for i in range(1,m):
             if obstacleGrid[i][0] != 1:
                 if obstacleGrid[i-1][0] == -1:obstacleGrid[i][0] = -1
-        print(obstacleGrid)
         for i in range(1,m):
             for j in range(1,n):
                 if obstacleGrid[i][j] != 1:
